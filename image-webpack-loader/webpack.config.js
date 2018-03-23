c078d40aaccb567cc0a0e76f5830459b015750b6
qingming <358242939@qq.com>
2018-03-23 13:42:21 +0800

    update

---
+++ image-webpack-loader/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+// mozjpeg — Compress JPEG images
+// optipng — Compress PNG images
+// pngquant — Compress PNG images
+// svgo — Compress SVG images
+// gifsicle — Compress GIF images
+// And optional optimizers:
+// webp — Compress JPG & PNG images into WEBP
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [{
+      test: /\.(gif|png|jpe?g|svg)$/i,
+      use: [
+        'file-loader',
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            bypassOnDebug: true,
+            mozjpeg: {
+              progressive: true,
+              quality: 65
+            },
+            // optipng.enabled: false will disable optipng
+            optipng: {
+              enabled: false,
+            },
+            pngquant: {
+              quality: '65-90',
+              speed: 4
+            },
+            gifsicle: {
+              interlaced: false,
+            },
+            // the webp option will enable WEBP
+            webp: {
+              quality: 75
+            }
+          }
+        },
+      ],
+    }]
+  }
+};
